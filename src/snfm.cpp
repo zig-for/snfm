d083146e7aece27fa549ec413fb3e844a492045a
Kyle Franz <san.francisco.engineer@gmail.com>
2022-06-19 23:38:57 -0700

    wip

--- src/snfm.cpp
+++ src/snfm.cpp
@@ -1,4 +1,17 @@
+#include "sni.pb.h"
+#include "sni.grpc.pb.h"
+
+#include <grpcpp/grpcpp.h>
+
 int main()
-{    
+{
+  const char address[] = "localhost:8191";
+    
+  std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+  auto stub = Devices::NewStub(channel);
+//service Devices {
+  // detect and list devices currently connected to the system:
+  //rpc ListDevices(DevicesRequest) returns (DevicesResponse) {}
+
 
 }
