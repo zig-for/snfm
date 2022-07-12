4a9cdaa934afdcb212d05d45677c6321240c3801
zig <san.francisco.engineer@gmail.com>
2022-07-11 19:15:35 -0700

    include icons lol

---
+++ include/icons.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#undef EXPORT
+
+enum class SystemIcon
+{
+    FOLDER = 3,
+    OPEN_FOLDER = 4,
+    QUESTION = 23,
+    RENAME = 133,
+    EXPORT = 122,
+    REFRESH = 238,
+    EXECUTE = 261,
+    RESET_TO_MENU = 253,
+    RESET_GAME = 46,
+};
+
+inline wxIcon GetSystemIcon(SystemIcon index)
+{
+#if WIN32
+
+    HICON hicon;
+    wxIcon icon;
+
+    ::ExtractIconEx(L"shell32.dll", (int)index, NULL, &hicon, 1);
+    icon.SetHandle(hicon);
+
+    return icon;
+#else
+    return {};
+#endif
+
+}
