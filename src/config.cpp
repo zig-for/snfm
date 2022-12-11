10695e4b966b897027b404dc969506efbc88a9ac
Kyle Franz <san.francisco.engineer@gmail.com>
2022-12-10 16:41:14 -0800

    fix

--- src/config.cpp
+++ src/config.cpp
@@ -28,6 +28,21 @@ std::optional<std::reference_wrapper<const RomDestinationRule>> Config::FindRule
     }
     return {};
 }
+std::vector<std::string> StringSplit(const std::string& s, const std::string& delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
 Config LoadConfig(const std::filesystem::path& path)
 {
     std::cout << "Loading config file at " << path << std::endl;
@@ -79,11 +94,12 @@ std::optional<std::filesystem::path> FindExistingConfigFile(const std::filesyste
 
     if (xdg_config_dirs)
     {
-        auto splits =
+     /*   auto splits =
             std::string_view(xdg_config_dirs) | std::views::split(':')
             | std::views::transform([](auto v) {
             return std::string_view(v.begin(), v.end());
-                });
+                });*/
+        auto splits = StringSplit(xdg_config_dirs, ":");
         for (const auto& d : splits) {
             dirs.push_back(std::filesystem::path(d) / "snfm");
         }
